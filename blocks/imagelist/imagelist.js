9fa1652118f886cb7260847dd276a3cbeac3a6df
vpokotylo <vadim@herodigital.com>
2023-05-17 21:25:01 -0500

    imagelist styling

--- blocks/imagelist/imagelist.js
+++ blocks/imagelist/imagelist.js
@@ -1,3 +1,48 @@
-export default async function decorate(block) {
-
-}
+function getMetadata(name, doc) {
+    const attr = name && name.includes(':') ? 'property' : 'name';
+    const meta = [...doc.head.querySelectorAll(`meta[${attr}="${name}"]`)].map((m) => m.content).join(', ');
+    return meta || '';
+  }
+  
+  async function loadFragment(path) {
+    if (path && path.startsWith('/')) {
+      const resp = await fetch(path);
+      if (resp.ok) {
+        const parser = new DOMParser();
+        return parser.parseFromString(await resp.text(), 'text/html');
+      }
+    }
+    return null;
+  }
+  
+  export default async function decorate(block) {
+    [...block.children].forEach(async (div) => {
+      const link = div.querySelector('div>div>a');
+      const path = link ? link.getAttribute('href') : block.textContent.trim();
+      const doc = await loadFragment(path);
+      div.remove();
+  
+      const heroPicture = doc.querySelector('picture');
+      const title = getMetadata('og:title', doc);
+      const desc = getMetadata('og:description', doc);
+  
+      const card = document.createElement('div');
+      card.classList.add('card');
+  
+      const h2 = document.createElement('h2');
+      h2.textContent = title;
+  
+      const p = document.createElement('p');
+      p.textContent = desc;
+  
+      card.appendChild(heroPicture);
+      card.appendChild(h2);
+      card.appendChild(p);
+  
+      const a = document.createElement('a');  
+      a.href = doc.querySelector('link').href;
+      a.appendChild(card);
+  
+      block.appendChild(a);
+    });
+  }
